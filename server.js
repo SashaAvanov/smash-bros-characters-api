08ae43589a69af48d0d7d2f33b9e873bd9b84eec
Sasha Avanov <sasha.avanov.webdeveloper@gmail.com>
2024-06-11 12:37:07 -0700

    fixed port

--- server.js
+++ server.js
@@ -58,6 +58,6 @@ app.get('/api/:name', (req, res) => {
     }
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`This api is now running on port ${PORT}`)
 })
