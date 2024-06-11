6f3f957fbe48f9d7d459784b25972e466cdd4a12
Sasha Avanov <sasha.avanov.webdeveloper@gmail.com>
2024-06-11 12:19:32 -0700

    first commit - working api

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const characters = {
+    'captain falcon': {
+        'name': 'Captain Falcon',
+        'gameSeries': 'F-Zero',
+        'specialMove': 'raptor boost'
+    },
+    'jigglypuff': {
+        'name': 'Jigglypuff',
+        'gameSeries': 'Pokemon',
+        'specialMove': 'rest'
+    },
+    'fox': {
+        'name': 'Fox',
+        'gameSeries': 'Starfox',
+        'specialMove': 'shine'
+    },
+    'falco': {
+        'name': 'Falco',
+        'gameSeries': 'Starfox',
+        'specialMove': 'shine'
+    },
+    'marth': {
+        'name': 'Marth',
+        'gameSeries': 'Fire Emblem',
+        'specialMove': 'dolphin slash'
+    },
+    'sheik': {
+        'name': 'Sheik',
+        'gameSeries': 'The Legend of Zelda',
+        'specialMove': 'needle'
+    },
+    'peach': {
+        'name': 'Peach',
+        'gameSeries': 'Super Mario Bros.',
+        'specialMove': 'turnip'
+    },
+    'unknown': {
+        'name': 'unknown',
+        'gameSeries': 'unknown',
+        'specialMove': 'unknown'
+    }
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'index.html')
+})
+
+app.get('/api/:name', (req, res) => {
+    const characterName = req.params.name.toLowerCase()
+    if (characters[characterName]) {
+        res.json(characters[characterName])
+    } else {
+        res.json(characters['unknown'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`This api is now running on port ${PORT}`)
+})
